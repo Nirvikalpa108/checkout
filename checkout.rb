520a022b9e9faeaf7674adce33e0607f6cf8e425
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-08-19 22:42:44 +0100

    adds extra line at end

--- checkout.rb
+++ checkout.rb
@@ -8,6 +8,10 @@ class CheckOut
   def total
     return 0 if @goods.empty?
 
+    # write logic for checking for special offers
+    # change the @goods array
+    # then implement the logic to sum all of the other products
+
     product = @goods.map { |item| @rules[item] }
     product.sum
   end
